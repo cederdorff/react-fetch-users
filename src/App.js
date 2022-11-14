13f365a8113dc06d4828ebd969deef717787bca1
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-14 22:45:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import { useEffect, useState } from "react";
 import Person from "./components/Person";
 
 function App() {
-    const [persons, setPersons] = useState([]);
+    const [persons, setPersons] = useState([]); // state to handle the data (persons)
+    // persons: name of the state
+    // setPersons: name of the function to set the state
 
     useEffect(() => {
         //the side effect - fetch persons/users
         async function getData() {
-            const response = await fetch("https://raw.githubusercontent.com/cederdorff/mdu-frontend/main/data/users.json");
+            const response = await fetch("https://race-crud-rest-default-rtdb.firebaseio.com/users.json"); // read all users (persons) from firebase
             const data = await response.json();
-            console.log(data);
-            setPersons(data);
+            const array = Object.keys(data).map(key => ({ id: key, ...data[key] })); // from object to array
+            setPersons(array); // set the state with fetched data
         }
 
         getData();
-    }, []);
+    }, []); // <--- "[]" VERY IMPORTANT!!!
 
     return (
-        <>
+        <div>
             <header className="topbar">
                 <h1>React Fetch Persons</h1>
             </header>
@@ -28,7 +30,7 @@ function App() {
                     ))}
                 </section>
             </main>
-        </>
+        </div>
     );
 }
 
